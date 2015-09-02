d2f57a0a567100270e41036a4d076fa37a353caa
Aryan Falahatpisheh <aryan.falahatpisheh@berkeley.edu>
2015-09-02 12:17:17 -0700

    Can now profile

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import numpy as np
+import datetime
 from matrix import Matrix
 import sys
-# O(2^n)
+import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+
 def memoize(fn):
 	seen = dict()
 	def to_return(*args):
@@ -10,7 +14,6 @@ def memoize(fn):
 		return seen[args]
 	return to_return;
 
-# O(n)
 def normal(n):
 	if n == 0:
 		return 0
@@ -19,10 +22,20 @@ def normal(n):
 	else:
 		return normal(n - 1) + normal(n - 2)
 
-# O(log(n))
 def dynamic(n):
-	fn = memoize(normal)
-	return fn(n);
+	if n == 0:
+		return 0
+
+	curr = 1
+	prev = 0
+
+	while n > 1:
+		prev, curr = curr, prev + curr
+		n -= 1
+
+	return curr
+
+	
 
 def fastest(n):
 	matrix = Matrix([[0, 1],[1, 1]])
@@ -30,22 +43,45 @@ def fastest(n):
 	ans = (matrix**n) * to_mult;
 	return ans.row(0)[0];
 
+def time_to_execute(fn, *args):
+	a = datetime.datetime.now()
+	fn(*args);
+	b = datetime.datetime.now()
+	c = b - a
+	return c.microseconds
+
+def profile(max_n):
+
+	df_dict = dict()
+	df_dict["dynamic"] = [time_to_execute(dynamic, i) for i in xrange(2, max_n)]
+	df_dict["fastest"] = [time_to_execute(fastest, i) for i in xrange(2, max_n)]
+	
+	df = pd.DataFrame(df_dict)
+	ax = df.plot()
+	fig = ax.get_figure()
+	fig.savefig('profile.png')
 
 def main():
 	arg = sys.argv[1]
-
+	if (arg == "profile"):
+		raw_n = sys.argv[2]
+	else:
+		raw_n = arg
 	try:
-		n = int(arg)
+		n = int(raw_n)
 	except ValueError:
-		print("Argument must be an integer! Found: {}".format(raw_n))
+		print("Argument must be an integer! Found: {}".format(arg))
 
+	if (arg == "profile"):
+		profile(n)
+		return
 	if (n <= 30):
 		print "Normal: {} ".format(normal(n))
 	else:
 		print("n too high for normal")
 
 
-	if (n <= 35):
+	if (n <= 1000000):
 		print "Dynamic: {} ".format(dynamic(n))
 	else:
 		print("n too high for dynamic")
